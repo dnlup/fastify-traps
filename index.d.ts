3936334cb2a0dffd8df7db2e0f5cd89438303fa1
Daniele Belardi <dwon.dnl@gmail.com>
2021-07-12 13:13:21 +0200

    feat: add typescript support

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+import {
+  FastifyPluginCallback,
+  FastifyPluginAsync,
+} from 'fastify'
+
+export interface TrapsPluginOptions {
+  timeout: number,
+  onSignal: (signal: 'SIGTERM' | 'SIGINT') => {},
+  onClose: () => {},
+  onTimeout: (timeout: number) => {},
+  onError: (error: Error|any) => {},
+  strict: boolean
+}
+
+export const trapsPluginCallback: FastifyPluginCallback<TrapsPluginOptions>;
+export const trapsPluginAsync: FastifyPluginAsync<TrapsPluginOptions>;
+
+export default trapsPluginCallback;
