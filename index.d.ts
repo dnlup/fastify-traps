763ee4bb5295607942e409ed9e52b8c32329f9d0
Mikael Karon <mikael@karon.se>
2021-09-02 23:28:38 +0200

    fix: Make plugin options optional

--- index.d.ts
+++ index.d.ts
@@ -4,12 +4,12 @@ import {
 } from 'fastify'
 
 export interface TrapsPluginOptions {
-  timeout: number,
-  onSignal: (signal: 'SIGTERM' | 'SIGINT') => {},
-  onClose: () => {},
-  onTimeout: (timeout: number) => {},
-  onError: (error: Error|any) => {},
-  strict: boolean
+  timeout?: number,
+  onSignal?: (signal: 'SIGTERM' | 'SIGINT') => {},
+  onClose?: () => {},
+  onTimeout?: (timeout: number) => {},
+  onError?: (error: Error|any) => {},
+  strict?: boolean
 }
 
 export const trapsPluginCallback: FastifyPluginCallback<TrapsPluginOptions>;
